3285fff4dae1571f749a16e8e6704d843c4ab36a
CzarPaul <55947136+CzarPaul@users.noreply.github.com>
2019-11-07 13:17:28 +0200

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+let startBtn = document.getElementById('start'),
+    budgetVal = document.querySelector('.budget-value'),
+    dayBudgetVal = document.querySelector('.daybudget-value'),
+    levelVal = document.querySelector('.level-value'),
+    expensesVal = document.querySelector('.expenses-value'),
+    optionalExpensesVal = document.querySelector('.optionalexpenses-value'),
+    incomeVal = document.querySelector('.income-value'),
+    monthSavingsVal = document.querySelector('.monthsavings-value'),
+    yearSavingsVal = document.querySelector('.yearsavings-value'),
+    expensesItem = document.querySelectorAll('.expenses-item'),
+    expensesItemBtn = document.getElementsByTagName('button'),
+    acceptBtn1 = document. querySelector('.expenses-item-btn'),
+    acceptBtn2 = document.querySelector('.optionalexpenses-btn'),
+    countBtn = document.querySelector('.count-budget-btn'),
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('#income'),
+    checkBox = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    yearVal = document.querySelector('.year-value'),
+    monthVal = document.querySelector('.month-value'),
+    dayVal = document.querySelector('.day-value');
+
+
+
+    console.log(startBtn);
+    console.log(budgetVal);
+    console.log(dayBudgetVal);
+    console.log(levelVal);
+    console.log(expensesVal);
+    console.log(optionalExpensesVal);
+    console.log(incomeVal);
+    console.log(monthSavingsVal);
+    console.log(yearSavingsVal);
+    console.log(expensesItem);
+    console.log(expensesItemBtn);
+    console.log(acceptBtn1);
+    console.log(acceptBtn2);
+    console.log(countBtn);
+    console.log(optionalExpensesItem);
+    console.log(chooseIncome);
+    console.log(checkBox);
+    console.log(sum);
+    console.log(percent);
+    console.log(yearVal);
+    console.log(monthVal);
+    console.log(dayVal);
